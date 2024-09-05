e7e3593f8eb1cf954bec0dfe7a53bb9694eaf717
KimYoungUng74 <xodnwong9596@gmail.com>
2024-09-06 00:23:33 +0900

    1157번 - 단어 공부
    - 구현
    - 문자열

--- src/baekJoon/Main.java
+++ src/baekJoon/Main.java
@@ -3,53 +3,42 @@ package baekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
-// 5073번 - 삼각형과 세 변
+// 1157번 - 단어공부
 public class Main {
 	public static void main(String[] args) throws IOException {
+	   // 가장 높은 빈도를 가진 알파벳 찾기
+       int maxFrequency = 0;
+       char mostFrequentChar = ' ';
+       Map<Character, Integer> charMap = new HashMap<>();
+	       
 	   // 입력을 효율적으로 처리하기 위해 BufferedReader를 사용하여 입력을 받음
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	   
-	   StringTokenizer str = null;
+	   // 빈도수 분석할 문자열 입력받음
+	   StringTokenizer str = new StringTokenizer(br.readLine());
+
+	   // 대소문자 구분하지 않기 위해 모든 알파뱃 대문자로 치환
+	   String word = str.nextToken().toUpperCase();
 	   
-	   int side1 = 0;
-	   int side2 = 0;
-	   int side3 = 0;
-	   int maxSide = 0;
-	   String resultStr = "";
-	    
-		while(true) {
-			str = new StringTokenizer(br.readLine());
-			side1 = Integer.parseInt(str.nextToken());
-			side2 = Integer.parseInt(str.nextToken());
-			side3 = Integer.parseInt(str.nextToken());
-		 
-			if(side1 == 0 && side2 == 0 && side3 == 0) { // 마지막 줄 입력
-				resultStr = resultStr.trim(); // 마지막 \n 제거 
-				break;
-			}
-		 
-			// 세 변중 가장 긴 변의 길이 구하기
-			if (side1 >= side2 && side1 >= side3) {
-				maxSide = side1;
-			} else if (side2 >= side1 && side2 >= side3) {
-				maxSide = side2;
-			} else {
-				maxSide = side3;
-			}
-		 
-			if(side1+side2+side3-maxSide <= maxSide) { 	// 가장 긴 변의 길이가 다른 두변의 합보다 크거나 같을 때 
-				resultStr += "Invalid\n";
-			} else if(side1==side2 && side1==side3){	// 모든 변의 길이가 같을 때 
-				resultStr += "Equilateral\n";
-			} else if(side1==side2 || side1==side3 || side2==side3) {	// 두변의 길이가 같을 때
-				resultStr += "Isosceles\n";
-			} else {	// 모든 변의 길이가 다를 때
-				resultStr += "Scalene\n";
-			}
-		}
-		// 답 출력
-		System.out.println(resultStr);
+	   // 문자열을 분해하여 빈도수 저장
+	   for(char c : word.toCharArray()) {
+		   charMap.put(c, charMap.getOrDefault(c, 0)+1);
+	   }
+	   
+	   for(Map.Entry<Character, Integer> c : charMap.entrySet()) {
+		   if(maxFrequency < c.getValue()) {
+			   maxFrequency = c.getValue();
+			   mostFrequentChar = c.getKey();
+		   } else if(maxFrequency == c.getValue()) { // 최고빈도수가 중복된다면 '?' 출력
+			   mostFrequentChar = '?';
+		   }
+	   }
+	   
+	   // 답 출력
+	   System.out.println(mostFrequentChar);
 	}
 }
