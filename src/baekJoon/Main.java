ce3ccdadfb6a3190ded609b6e4c70eaad72d86de
KimYoungUng74 <xodnwong9596@gmail.com>
2024-09-06 00:18:30 +0900

    2292번 - 벌집
    알고리즘 분류
    - 수학

--- src/baekJoon/Main.java
+++ src/baekJoon/Main.java
@@ -5,24 +5,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//23971번 - ZOAC 4
+// 2292번 - 벌집
 public class Main {
-
- public static void main(String[] args) throws IOException {
-     // 입력을 효율적으로 처리하기 위해 BufferedReader를 사용하여 입력을 받음
-     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-     
-     // [행, 열, 세로공백, 가로공백]을 한줄로 입력 받음
-     StringTokenizer str = new StringTokenizer(br.readLine());
-     int row = Integer.parseInt(str.nextToken()); 	// 테이블 행 H
-     int column = Integer.parseInt(str.nextToken()); // 테이블 열 W
-     int rowGap = Integer.parseInt(str.nextToken()); // 참가자 세로 공백 N
-     int columnGap = Integer.parseInt(str.nextToken()); // 참가자 가로 공백 M
-     
-     // 최대 참가자 수 구하는 공식
-     int result = ((row-1)/(rowGap+1)+1) * ((column-1)/(columnGap+1)+1);
-     
-     // 답 출력
-     System.out.println(result);
- }
+	public static void main(String[] args) throws IOException {
+	   // 입력을 효율적으로 처리하기 위해 BufferedReader를 사용하여 입력을 받음
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   
+	   // 목적지 방의 숫자 입력받음
+	   StringTokenizer str = new StringTokenizer(br.readLine());
+	   
+	   // 육각형의 여러층 구조인 방들이 있다고 생각하고 층이 증가할때마다 기존 층 + 6개의 방이 존재한다.
+	   // 예를 들어 1층 1번방 -> 2층 2~7번까지 6개의 방이 존재 3층 8~19번 12개의 방이 존재 
+	   int honeycomb = Integer.parseInt(str.nextToken());	// 목적지 방의 숫자
+	   int rage = 1;	// 지나온 층의 방 범위
+	   int count = 1;	// 시작과 끝을 포함하여 지나가는 방의 개수
+	   
+	   if(honeycomb == 1) {
+		   System.out.print(1);
+	   } else {
+		   while(honeycomb > rage) {
+			   rage += count*6; // 층이 올라갈수록 기존 층 개수 + 6개 의 방이 신규로 존재
+			   count++;
+		   }
+		   
+		   // 답 출력
+		   System.out.print(count);
+	   }
+	}
 }
