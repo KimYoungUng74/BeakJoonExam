7b4a5abbe251e8b5da5f5756643cca2982f56aa4
KimYoungUng74 <xodnwong9596@gmail.com>
2024-09-06 00:20:55 +0900

    5073번 - 삼각형과 세 변
    알고리즘 분류
    - 수학

--- src/baekJoon/Main.java
+++ src/baekJoon/Main.java
@@ -5,31 +5,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 2292번 - 벌집
+// 5073번 - 삼각형과 세 변
 public class Main {
 	public static void main(String[] args) throws IOException {
 	   // 입력을 효율적으로 처리하기 위해 BufferedReader를 사용하여 입력을 받음
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	   
-	   // 목적지 방의 숫자 입력받음
-	   StringTokenizer str = new StringTokenizer(br.readLine());
+	   StringTokenizer str = null;
 	   
-	   // 육각형의 여러층 구조인 방들이 있다고 생각하고 층이 증가할때마다 기존 층 + 6개의 방이 존재한다.
-	   // 예를 들어 1층 1번방 -> 2층 2~7번까지 6개의 방이 존재 3층 8~19번 12개의 방이 존재 
-	   int honeycomb = Integer.parseInt(str.nextToken());	// 목적지 방의 숫자
-	   int rage = 1;	// 지나온 층의 방 범위
-	   int count = 1;	// 시작과 끝을 포함하여 지나가는 방의 개수
-	   
-	   if(honeycomb == 1) {
-		   System.out.print(1);
-	   } else {
-		   while(honeycomb > rage) {
-			   rage += count*6; // 층이 올라갈수록 기존 층 개수 + 6개 의 방이 신규로 존재
-			   count++;
-		   }
-		   
-		   // 답 출력
-		   System.out.print(count);
-	   }
+	   int side1 = 0;
+	   int side2 = 0;
+	   int side3 = 0;
+	   int maxSide = 0;
+	   String resultStr = "";
+	    
+		while(true) {
+			str = new StringTokenizer(br.readLine());
+			side1 = Integer.parseInt(str.nextToken());
+			side2 = Integer.parseInt(str.nextToken());
+			side3 = Integer.parseInt(str.nextToken());
+		 
+			if(side1 == 0 && side2 == 0 && side3 == 0) { // 마지막 줄 입력
+				resultStr = resultStr.trim(); // 마지막 \n 제거 
+				break;
+			}
+		 
+			// 세 변중 가장 긴 변의 길이 구하기
+			if (side1 >= side2 && side1 >= side3) {
+				maxSide = side1;
+			} else if (side2 >= side1 && side2 >= side3) {
+				maxSide = side2;
+			} else {
+				maxSide = side3;
+			}
+		 
+			if(side1+side2+side3-maxSide <= maxSide) { 	// 가장 긴 변의 길이가 다른 두변의 합보다 크거나 같을 때 
+				resultStr += "Invalid\n";
+			} else if(side1==side2 && side1==side3){	// 모든 변의 길이가 같을 때 
+				resultStr += "Equilateral\n";
+			} else if(side1==side2 || side1==side3 || side2==side3) {	// 두변의 길이가 같을 때
+				resultStr += "Isosceles\n";
+			} else {	// 모든 변의 길이가 다를 때
+				resultStr += "Scalene\n";
+			}
+		}
+		// 답 출력
+		System.out.println(resultStr);
 	}
 }
